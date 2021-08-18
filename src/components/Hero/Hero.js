d4144269146053713b8875c90a1bb04165a2422c
Robert Radzville <rradzvil@ucsc.edu>
2021-08-18 15:42:51 -0700

    Project layouts complete

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello! <br />
+        I'm Robbie
+      </SectionTitle>
+      <SectionText>
+        We the best music
+      </SectionText>
+      <Button onClick={()=> window.location - 'https://google.com'}>Yeet</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
