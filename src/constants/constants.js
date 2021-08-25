7121b3cf252bdb4added7de23a2ceed449071a4d
Robert Radzville <rradzvil@ucsc.edu>
2021-08-25 11:24:59 -0700

    Style changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,16 @@
 export const projects = [
   {
+    title: 'Phaser Games',
+    description:"A collection of games created with the Phaser engine using JavaScript. These were created with a team of 4 people, " +
+    "including 2 artists and 2 programmers who also created the audio and the UI. I was responsible for coding much of the player controls" + 
+    ", interactable objects, and UI.",
+    image: '/images/JSgame.png',
+    tags: ['Phaser', 'JavaScript', 'Github Pages'],
+    source: 'https://robradz.github.io/Endless-Runner/',
+    visit: 'https://github.com/Robradz/Endless-Runner',
+    id: 1,
+  },
+  {
     title: 'Unity Games',
     description: "A collection of games created with the Unity engine using C# scripts. These games explore 5 different genres. The first 4 were created from start to finish by myself with the help of Gamedev.tv's 'Complete C# Unity Game Developer 3D' course. The newest game was created for the 2021 Global Game Jam with team of 4 people, in which I was responsible for the physics of the character movement and the level layout.",
     image: '/images/itch.png',
@@ -9,17 +20,8 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'Phaser Games',
-    description:"A collection of games created with the Phaser engine using JavaScript. These were created with a team of 4 people, including 2 artists and 2 programmers who also created the audio and the UI.",
-    image: '/images/JSgame.png',
-    tags: ['Phaser', 'JavaScript', 'Github Pages'],
-    source: 'https://robradz.github.io/Endless-Runner/',
-    visit: 'https://github.com/Robradz/Endless-Runner',
-    id: 1,
-  },
-  {
     title: 'Elemental Models',
-    description:"A graphics demonstration using ShaderLab, a language similar to GLSL. This was made as part of a group project with 2 other students. I was responcible for the changing and mapping of the textures (using ShaderLab), the particle systems (using C#), and the background.",
+    description:"A graphics demonstration using ShaderLab, a language similar to GLSL. This was made as part of a group project with 2 other students. I was responsible for the changing and mapping of the textures (using ShaderLab), the particle systems (using C#), and the background.",
     image: '/images/ElementalModels_Moment.jpg',
     tags: ['ShaderLab', 'C#', 'Unity', 'Visual Studio'],
     source: 'https://youtu.be/d4jzIQ_Ki3M',
